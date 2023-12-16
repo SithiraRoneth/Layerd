d2b74c8fa43f950f547a86cd7fa0d87fe6fb5c02
USER <sithiraroneth@gmail.com>
2023-12-16 21:56:56 +0530

    first commit

--- src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
+++ src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
@@ -26,7 +26,6 @@ import java.math.BigDecimal;
 import java.net.URL;
 import java.sql.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
