b7f454a1db1626f71e994f518930190c819cdc29
USER <sithiraroneth@gmail.com>
2023-12-18 22:15:59 +0530

    initial commit

--- src/main/java/com/example/layeredarchitecture/DAO/CustomerDAO.java
+++ src/main/java/com/example/layeredarchitecture/DAO/CustomerDAO.java
@@ -1,6 +1,5 @@
 package com.example.layeredarchitecture.DAO;
 
-import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.CustomerDTO;
 
 import java.sql.*;
---
+++ src/main/java/com/example/layeredarchitecture/controller/SearchOrderController.java
@@ -0,0 +1,9 @@
+/* Created By Sithira Roneth
+ * Date :12/18/23
+ * Time :22:13
+ * Project Name :working
+ * */
+package com.example.layeredarchitecture.controller;
+
+public class SearchOrderController {
+}
